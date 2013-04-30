bb4b78619de34ae9221c0a16ef4730014d9fe2e4
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-30 16:53:48 +0500

    pgconnect changes

--- pgconnect.php
+++ pgconnect.php
@@ -2,20 +2,43 @@
 $dbconnection = pg_connect("host=localhost port=5432 dbname=mint_ams user=mint password=mint");
 if($dbconnection)
 {
-        $result=pg_query($dbconnection,"SELECT * FROM users");
-        if($result)
-        {
-                $arr=pg_fetch_all($result);
-				$response = json_encode(array("success" => 'true', 'error' => '','result'=>$arr));
+	if(isset($_REQUEST['mint_id'])){
+		if($_REQUEST['mint_id']==''){
+			// insert
+		}
+		else{
+			$user_id=$_REQUEST['mint_id'];
+	        $result=pg_query($dbconnection,"SELECT * FROM users WHERE users_id = $user_id");
+	        if($result)
+	        {
+	                $user_info=pg_fetch_all($result);
+					$response = json_encode(array("success" => 'true', 'error' => '','result'=>$user_info));
+					echo $_GET['callback'] . '(' . $response . ')';
+					exit;
+	        }
+			else
+			{
+				$username = explode('@',$_REQUEST['email']);
+				$data=array(
+					'rights'=>4,
+					'organization_id'=>1,
+					'login'=>$username[0],
+					'first_name'=>$_REQUEST['first_name'],
+					'last_name'=>$_REQUEST['last_name'],
+					'email'=>$username,
+					'md5_password'=>md5($username[0]),
+					'account_created'=>date('Y-m-d'),
+					'active_account'=>'t'
+				);
+				$result = pg_insert($dbconnection, 'users', $data);
+				$response = json_encode(array("success" => 'true', 'error' => '','result'=>$result));
 				echo $_GET['callback'] . '(' . $response . ')';
 				exit;
-        }
-		else
-		{
-			$response = json_encode(array("success" => 'false', 'error' => 'No Record found.'));
-			echo $_GET['callback'] . '(' . $response . ')';
-			exit;
+			}
 		}
+		
+	}
+       
 }
 else
 {
