d2c555862012351e163d035e7502cd8b7c34a647
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-30 17:00:15 +0500

    pgconnect changes

--- pgconnect.php
+++ pgconnect.php
@@ -9,7 +9,7 @@ if($dbconnection)
 				'login'=>$username[0],
 				'first_name'=>$_REQUEST['first_name'],
 				'last_name'=>$_REQUEST['last_name'],
-				'email'=>$username,
+				'email'=>$_REQUEST['email'],
 				'md5_password'=>md5($username[0]),
 				'organization_id'=>1,
 				'account_created'=>date('Y-m-d'),
