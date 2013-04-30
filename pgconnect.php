603e274953c2324488b1102792742b2560205cc3
Nouman Tayyan <noumantayyab@gmail.com>
2013-04-30 16:58:41 +0500

    pgconnect changes

--- pgconnect.php
+++ pgconnect.php
@@ -4,7 +4,23 @@ if($dbconnection)
 {
 	if(isset($_REQUEST['mint_id'])){
 		if($_REQUEST['mint_id']==''){
-			// insert
+			$username = explode('@',$_REQUEST['email']);
+			$data=array(
+				'login'=>$username[0],
+				'first_name'=>$_REQUEST['first_name'],
+				'last_name'=>$_REQUEST['last_name'],
+				'email'=>$username,
+				'md5_password'=>md5($username[0]),
+				'organization_id'=>1,
+				'account_created'=>date('Y-m-d'),
+				'active_account'=>'t',
+				'rights'=>4
+			);
+			$result = pg_insert($dbconnection, 'users', $data);
+			$response = json_encode(array("success" => 'true', 'error' => '','result'=>$result));
+			echo $_GET['callback'] . '(' . $response . ')';
+			exit;
+			
 		}
 		else{
 			$user_id=$_REQUEST['mint_id'];
@@ -16,25 +32,7 @@ if($dbconnection)
 					echo $_GET['callback'] . '(' . $response . ')';
 					exit;
 	        }
-			else
-			{
-				$username = explode('@',$_REQUEST['email']);
-				$data=array(
-					'rights'=>4,
-					'organization_id'=>1,
-					'login'=>$username[0],
-					'first_name'=>$_REQUEST['first_name'],
-					'last_name'=>$_REQUEST['last_name'],
-					'email'=>$username,
-					'md5_password'=>md5($username[0]),
-					'account_created'=>date('Y-m-d'),
-					'active_account'=>'t'
-				);
-				$result = pg_insert($dbconnection, 'users', $data);
-				$response = json_encode(array("success" => 'true', 'error' => '','result'=>$result));
-				echo $_GET['callback'] . '(' . $response . ')';
-				exit;
-			}
+			
 		}
 		
 	}
