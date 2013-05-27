dcd47c92c7d4913a8ef7cf8f3f53d7459c40478c
Nouman Tayyan <noumantayyab@gmail.com>
2013-05-27 15:41:05 +0500

    xsd secnahea

--- src/main/java/gr/ntua/ivml/mint/xsd/XSDParser.java
+++ src/main/java/gr/ntua/ivml/mint/xsd/XSDParser.java
@@ -211,7 +211,8 @@ public class XSDParser {
 					|| namespace.equals("http://www.w3.org/XML/1998/namespace")) {
 				prefix = "xml";
 			} else {
-				prefix = "pr" + this.namespaces.keySet().size();
+//				prefix = "pr" + this.namespaces.keySet().size();
+				prefix = "";
 			}
 
 			this.namespaces.put(namespace, prefix);
